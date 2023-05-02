90349c06927d5d104e75d13d449088f99eb78256
mgst-d <and8511@ya.ru>
2023-05-02 14:47:10 +0300

    Add model Article

---
+++ db/migrate/20230502114349_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.text :title
+      t.text :description
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/articles.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :article do
+    title { "MyText" }
+    description { "MyText" }
+    body { "MyText" }
+  end
+end
